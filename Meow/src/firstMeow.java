c37362a1c26b1aac04eb55937963c393ba52a529
jidrids <jidrids@gmail.com>
2013-09-11 15:03:39 +0700

    Meow Meow

---
+++ Meow/src/firstMeow.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class firstMeow {
+	public static void main(String[] args)
+	{
+		System.out.println("INPUT HOW MUCH MEOW?");
+		Scanner scanner = new Scanner(System.in);
+		int meow = scanner.nextInt();
+		for(int meow2=0;meow2<=meow;meow2++)
+		{
+			System.out.println("Meow ");
+		}
+	}
+}
